f920b21d4bb739a9b504306a3f7e5654f6e37154
PressureDraper <warmarines00@hotmail.com>
2022-04-24 15:11:53 -0500

    creating endpoint that returns explorers list

--- app/app.js
+++ app/app.js
@@ -10,3 +10,13 @@ const port = 3000
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
 })
+
+app.get('/v1/explorers', (req, res) => {
+    console.log(`API Explorers GET ALL requests ${new Date()}`)
+    const explorer1 = {id: 1, name: "Sahib1"}
+    const explorer2 = {id: 2, name: "Sahib2"}
+    const explorer3 = {id: 3, name: "Sahib3"}
+    const explorer4 = {id: 4, name: "Sahib4"}
+    const explorers = [explorer1, explorer2, explorer3, explorer4]
+    res.status(200).json(explorers)
+})
